9072486c09a88fa55f5dd9b24e5c2c8b4ad528f2
npipkins <68622679+npipkins@users.noreply.github.com>
2020-09-15 16:20:35 -0500

    part II

--- VacationsPy/api_keys.py
+++ VacationsPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "43bb5552c7f2a34330d2a7cea049312c"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAZA2FtQRu2RCig_SILt6zNtd9FOq6ZEno"
